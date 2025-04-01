9e666c03f8e1792985148c981a4cb452c68055c0
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2025-04-01 03:29:01 +0100

    feat: config whitenoise for serving static files

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ python manage.py migrate blog
 
 python manage.py makemigrations
 python manage.py migrate
+
+python manage.py collectstatic
--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -53,6 +53,7 @@ DB_PREFIX = "retweeter_"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -133,6 +134,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Whitenoise backend compression and caching
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
